5ad8d0f9b37b009b6eee89d8a6897db9af38779d
SuperKXT <superkxt@outlook.com>
2024-04-14 16:09:52 +0500

    Update `theme`

--- src/theme.ts
+++ src/theme.ts
@@ -4,10 +4,13 @@ import './theme.css';
 export const theme: DeckProps['theme'] = {
 	colors: {
 		primary: 'var(--purple)',
-		secondary: 'var(--red)',
+		secondary: 'var(--fg)',
 		tertiary: 'var(--bg)',
 		quaternary: 'var(--green)',
 		quinary: 'var(--blue)',
+		// @ts-expect-error bad TS types
+		bg: 'var(--bg)',
+		fg: 'var(--fg)',
 	},
 	fonts: {
 		header: '"lexend-bold", sans-serif',
